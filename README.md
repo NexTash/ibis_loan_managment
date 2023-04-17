bd2284a8d8a2c9b56655e5312d0efcb1eeaba56b
tania_nawaz <tanianawaz.7869@gmail.com>
2023-04-18 03:16:28 +0500

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Ibis Loan Managment
+
+this app is for loan managment service
+
+#### License
+
+MIT
---
+++ ibis_loan_managment/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Ibis Loan Managment",
+			"color": "grey",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("Ibis Loan Managment")
+		}
+	]
